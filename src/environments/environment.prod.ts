14da342f6fac1f7a0271d31d1c6ddcff48331a86
Mutnalsaksha <mutnalsaksha@gmail.com>
2024-03-11 12:59:38 +0530

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://websitebackend-1.onrender.com', // Replace with your hosted backend URL
+    apiUrl: 'https://backend-1-fxkv.onrender.com', // Replace with your hosted backend URL
   };
-  
