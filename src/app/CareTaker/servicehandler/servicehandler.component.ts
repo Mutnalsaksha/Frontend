3b4ac776c744675debaeb4945fe553d98a598a4e
Mutnalsaksha <mutnalsaksha@gmail.com>
2024-05-06 12:05:21 +0530

    Add Login and Service Handler component

---
+++ src/app/CareTaker/servicehandler/servicehandler.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicehandler',
+  templateUrl: './servicehandler.component.html',
+  styleUrls: ['./servicehandler.component.css']
+})
+export class ServicehandlerComponent {
+
+}
